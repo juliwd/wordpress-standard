fbad34c9342e3f3c3e5a9a878f5f0b13dc71f42a
Juliano Amaral <juliano@ethymos.com.br>
2013-07-05 11:15:32 -0300

    css and js folter with functions file

---
+++ functions.php
@@ -0,0 +1,27 @@
+<?php 
+
+/*
+| Stylesheets
+|----------------------------------------------------------------------
+*/
+
+function add_stylesheets()
+{
+	// add here your css files
+	wp_enqueue_style('normalize', get_stylesheet_directory_uri().'/css/normalize.css');
+}
+
+add_action('wp_enqueue_styles', 'add_stylesheets');
+
+/*
+| Javascripts
+|----------------------------------------------------------------------
+*/
+
+function add_stylesheets()
+{
+	// add here your js files
+	wp_enqueue_script('jquery', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/1.10.2/jquery.min.js');
+}
+
+add_action('wp_enqueue_scripts', 'add_stylesheets');
